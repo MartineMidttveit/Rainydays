139bc624b4cd9c73b8b9c22637a16af9511ef069
Martine23FT <martinemidttveit@hotmail.com>
2023-09-14 20:06:33 +0200

    Changes

--- JS/createHTML/sectionFour.js
+++ JS/createHTML/sectionFour.js
@@ -42,16 +42,16 @@ export default function sectionFour (jacket, newProduct) {
 
     const list = document.createElement("ul");
 
-   const item1 = document.createElement("li");
-   item1.textContent = "Adjustable hoodie";
-   const item2 = document.createElement("li");
-   item2.textContent = "Waterproof zipper";
-   const item3 = document.createElement("li");
-   item3.textContent = "Wind-resistant";
-   const item4 = document.createElement("li")
-   item4.textContent = "Two vertical pockets";
-   const item5 = document.createElement("li")
-   item5.textContent = "Ventilating zipper";
+    const item1 = document.createElement("li");
+    item1.textContent = "Adjustable hoodie";
+    const item2 = document.createElement("li");
+    item2.textContent = "Waterproof zipper";
+    const item3 = document.createElement("li");
+    item3.textContent = "Wind-resistant";
+    const item4 = document.createElement("li")
+    item4.textContent = "Two vertical pockets";
+    const item5 = document.createElement("li")
+    item5.textContent = "Ventilating zipper";
 
     list.append(item1,item2,item3,item4,item5)
     
@@ -64,25 +64,3 @@ export default function sectionFour (jacket, newProduct) {
 
     newProduct.append(container);
 }
-
-
-
-{/* 
-    <div class="container">
-        <h1 class="big-title">Thunderbolt Jacket.</h1>
-        <p>
-            Introducing our rugged Thunderbolt Jacket, the ultimate companion for all 
-            your outdoor adventures. Crafted with cutting-edge waterproof technology, this jacket 
-            ensures complete protection against rain and damp conditions, keeping you dry and 
-            comfortable. Its durable design, breathable fabric, and versatile style make it a 
-            must-have for anyone seeking reliable performance and style in the great outdoors.
-        </p>
-        <ul>
-            <li>Adjustable hoodie</li>
-            <li>Waterproof zipper</li>
-            <li>Wind-resistant</li>
-            <li>Two vertical pockets</li>
-            <li>Ventilating zipper</li>
-        </ul>
-        <a href="/HTML/jacket.html" class="dark-green-btn">SHOP NOW</a>
-    </div> */}
