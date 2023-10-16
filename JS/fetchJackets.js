d5a0857253f48ac1e79637de0a7960de83272f40
Martine23FT <martinemidttveit@hotmail.com>
2023-10-16 22:54:42 +0200

    Update fetchJackets.js

--- JS/fetchJackets.js
+++ JS/fetchJackets.js
@@ -9,3 +9,7 @@ export default async function fetchJackets (url, id = "") {
         return error;
     }
 }
+
+// const consumerKey = 'ck_3d78534cad4d9042bcd2d574e6322f7a5b1b10ba';
+// const consumerSecret = 'cs_efbe898e78435935168e51ccb78e2471bd411be9';
+// const credentials = btoa(consumerKey + ':' + consumerSecret);
