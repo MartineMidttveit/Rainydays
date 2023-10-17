5589272b2e5865d7a59c9f89ac48a17fbf59a508
Martine23FT <martinemidttveit@hotmail.com>
2023-10-17 17:57:39 +0200

    Changes

--- JS/details/subscribeSuccess.js
+++ JS/details/subscribeSuccess.js
@@ -16,28 +16,3 @@ export default function subscribeSuccess(email) {
     
 
 }
-
-// Your discount code will be sent to the provided email-address, huhuhu@hotmai.com, and in the square below. Happy shopping! 
-
-
-// h2 = .medium-title 
-// h1 = .big-title 
-// p = .newsletter-text
-// form = .subscription
-// grønn boks = id="discountCode"
-
-
-{/* <h2 class="medium-title">GET 10% DISCOUNT</h2>
-        <h1 class="big-title">JOIN OUR ADVENTURES.</h1>
-        <p>
-            Sign up for our newsletter today and be the first to hear about our latest jacket 
-            designs and exclusive offers. As a special welcome gift, you'll receive a 10% discount 
-            on your next order. Sign up now and embrace the rainy days!
-        </p>
-
-        <form class="subscription">
-            <input type="text" placeholder="example-email@email.com" id="emailAddress">
-            <button class="dark-green-btn" id="subscribe">SUBSCRIBE</button>
-        </form> */}
-
-// dette skal inni .newsletter, i stedet for det er som er der nå. 
